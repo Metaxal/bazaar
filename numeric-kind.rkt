1b2e44d357b376164729299938a16a959f6ce9d8
Metaxal <laurent.orseau@gmail.com>
2015-06-17 14:37:47 +0100

    use parameter for numeric-kind

--- numeric-kind.rkt
+++ numeric-kind.rkt
@@ -7,10 +7,23 @@
          math/bigfloat
          racket/unsafe/ops)
 
-(provide with-numeric-kind)
+(provide with-numeric-kind
+         with-current-numeric-kind
+         current-numeric-kind
+         [rename-out [get-op $op]])
 
 (module+ test
   (require rackunit))
+
+(define current-numeric-kind
+  (make-parameter
+   'real
+   (λ(kind) (if (numeric-kind-index kind)
+                kind
+                (error 'current-numeric-kind
+                       "Unrecognized numeric kind: ~a. Existing kinds are ~a\n"
+                       kind
+                       (dict-ref op-dict '$kind))))))
 
 ;; In case we receive a float, convert it.
 (define (fx x)
@@ -21,19 +34,20 @@
 
 (define op-dict
   (list
-   (list '$?    real?   fixnum?     flonum?  extflonum?      bigfloat?         fixnum?            flonum?       extflonum?)
-   (list '$     values  fx          fl       real->extfl     bf                unsafe-fx          fl            real->extfl) ; real->$
-   (list '$->   values  fx->fl      values   extfl->inexact  bigfloat->flonum  unsafe-fx->fl      values        extfl->inexact) ; $->real
-   (list '$+    +       fx+         fl+      extfl+          bf+               unsafe-fx+         unsafe-fl+    unsafe-extfl+)
-   (list '$-    -       fx-         fl-      extfl-          bf-               unsafe-fx-         unsafe-fl-    unsafe-extfl-)
-   (list '$*    *       fx*         fl*      extfl*          bf*               unsafe-fx*         unsafe-fl*    unsafe-extfl*)
-   (list '$/    /       fxquotient  fl/      extfl/          bf/               unsafe-fxquotient  unsafe-fl/    unsafe-extfl/)
-   (list '$=    =       fx=         fl=      extfl=          bf=               unsafe-fx=         unsafe-fl=    unsafe-extfl=)
-   (list '$>    >       fx>         fl>      extfl>          bf>               unsafe-fx>         unsafe-fl>    unsafe-extfl>)
-   (list '$<    <       fx<         fl<      extfl<          bf<               unsafe-fx<         unsafe-fl<    unsafe-extfl<)
-   (list '$>=   >=      fx>=        fl>=     extfl>=         bf>=              unsafe-fx>=        unsafe-fl>=   unsafe-extfl>=)
-   (list '$<=   <=      fx<=        fl<=     extfl<=         bf<=              unsafe-fx<=        unsafe-fl<=   unsafe-extfl<=)
-   (list '$abs  abs     fxabs       flabs    extflabs        bfabs             unsafe-fxabs       unsafe-flabs  unsafe-extflabs)
+   (list '$kind  'real   'fixnum     'flonum  'extflonum      'bigfloat         'unsafe-fixnum     'unsafe-flonum  'unsafe-extflonum) ; $kind holds the symbolic kind
+   (list '$?     real?   fixnum?     flonum?  extflonum?      bigfloat?         fixnum?            flonum?         extflonum?)
+   (list '$      values  fx          fl       real->extfl     bf                unsafe-fx          fl              real->extfl) ; real->$
+   (list '$->    values  fx->fl      values   extfl->inexact  bigfloat->flonum  unsafe-fx->fl      values          extfl->inexact) ; $->real
+   (list '$+     +       fx+         fl+      extfl+          bf+               unsafe-fx+         unsafe-fl+      unsafe-extfl+)
+   (list '$-     -       fx-         fl-      extfl-          bf-               unsafe-fx-         unsafe-fl-      unsafe-extfl-)
+   (list '$*     *       fx*         fl*      extfl*          bf*               unsafe-fx*         unsafe-fl*      unsafe-extfl*)
+   (list '$/     /       fxquotient  fl/      extfl/          bf/               unsafe-fxquotient  unsafe-fl/      unsafe-extfl/)
+   (list '$=     =       fx=         fl=      extfl=          bf=               unsafe-fx=         unsafe-fl=      unsafe-extfl=)
+   (list '$>     >       fx>         fl>      extfl>          bf>               unsafe-fx>         unsafe-fl>      unsafe-extfl>)
+   (list '$<     <       fx<         fl<      extfl<          bf<               unsafe-fx<         unsafe-fl<      unsafe-extfl<)
+   (list '$>=    >=      fx>=        fl>=     extfl>=         bf>=              unsafe-fx>=        unsafe-fl>=     unsafe-extfl>=)
+   (list '$<=    <=      fx<=        fl<=     extfl<=         bf<=              unsafe-fx<=        unsafe-fl<=     unsafe-extfl<=)
+   (list '$abs   abs     fxabs       flabs    extflabs        bfabs             unsafe-fxabs       unsafe-flabs    unsafe-extflabs)
    ; to complete...
    ))
 
@@ -43,17 +57,22 @@
           [(member e (car l) =) idx]
           [else (loop (cdr l) (+ idx 1))])))
 
-(define (get-op op-sym kind)
-  (define idx
-    (subindex kind '((default generic real #f)
-                     (fixnum fx)
-                     (flonum fl)
-                     (extflonum extfl)
-                     (bigfloat bf)
-                     (unsafe-fixnum unsafe-fx)
-                     (unsafe-flonum unsafe-fl)
-                     (unsafe-extflonum unsafe-extfl)
-                     )))
+(define numeric-kinds
+  '((default generic real #f)
+    (fixnum fx)
+    (flonum fl)
+    (extflonum extfl)
+    (bigfloat bf)
+    (unsafe-fixnum unsafe-fx)
+    (unsafe-flonum unsafe-fl)
+    (unsafe-extflonum unsafe-extfl)
+    ))
+
+(define (numeric-kind-index kind)
+  (subindex kind numeric-kinds))
+
+(define (get-op op-sym [kind (current-numeric-kind)])
+  (define idx (numeric-kind-index kind))
   (unless idx
     (error "Could not find numeric kind: " kind))
   (list-ref (dict-ref op-dict op-sym
@@ -63,19 +82,36 @@
                                  (dict-keys op-dict))))
             idx))
 
+(define-syntax-rule (with-current-numeric-kind (op ...) body ...)
+  (let ([op (get-op 'op)] ...)
+      body ...))
+
 (define-syntax-rule (with-numeric-kind num-kind (op ...) body ...)
-  (let ([op (get-op 'op num-kind)] ...)
-    body ...))
+  (parameterize ([current-numeric-kind num-kind])
+    (with-current-numeric-kind (op ...) body ...)))
 
 (module+ test
   (define (test-kind kind)
     (with-numeric-kind
-     kind ($ $? $= $+ $- $* $/ $> $< $>= $<=)
+     kind ($kind $ $? $= $+ $- $* $/ $> $< $>= $<=)
+     (check-eq? $kind kind)
      (define n ($* ($/ ($- ($+ ($ 3) ($ 2)) ($ 15))
                        ($ 2))
                    ($ 4)))
      (check-pred $? n)
      (check $= n ($ -20))))
-  (for ([kind '(real fx fl extfl bf unsafe-fx unsafe-fl unsafe-extfl)])
-    (test-kind kind)))
+  (for ([kind (dict-ref op-dict '$kind)])
+    (test-kind kind))
 
+  (check-equal?
+   (with-numeric-kind
+    'bigfloat ($ $+)
+    ($+ ($ 3) ($ 4)))
+   (bf 7))
+
+  )
+
+;; TODO:
+;; - Can modify the '$' prefix? (something like '_' can be more readable sometimes)
+;;   (also to avoid collision with other modules)
+;; - define-numeric-kind to avoid the indentation shift ?
