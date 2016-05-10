e919fcecf871fce8654f99c21f1068be66763157
Metaxal <laurent.orseau@gmail.com>
2016-05-10 15:34:56 +0100

    update high-contrast palette generator, can now use a background color. Rename spread-palette to white palette

--- plot.rkt
+++ plot.rkt
@@ -13,7 +13,8 @@
 (define (build-points f a [b #f] [inc 1])
   (let ([a (if b a 1)]
         [b (if b b a)])
-    (for/list ([i (in-range a (+ b 1) inc)])
+    (for/list ([i (in-range a (+ b 1) inc)]
+               #:break (> i b))
       (list i (f i)))))
 
 (module+ test
@@ -26,6 +27,8 @@
                 '((1 1) (3 9)))
   (check-equal? (build-points sqr 4 #f 2)
                 '((1 1) (3 9)))
+  (check-equal? (length (build-points values 0 1 .2))
+                6)
   )
 
 ;; Useful for calls like:
@@ -36,6 +39,8 @@
    (build-points (λ(x)(* x x)) 1 10 2)
    '((1 1) (3 9) (5 25) (7 49) (9 81))))
 
+;; TODO: add a #:inc-skip argument that skips several items in the list
+;; (useful to build light-weight plots)
 (define (list->points l [start 0])
   (for/list ([x l] [i (in-naturals start)])
     (list i x)))
@@ -74,4 +79,294 @@
                  [plot-x-ticks (log-ticks)]
                  [plot-y-ticks (log-ticks)])
     body ...))
+
+;; A nicer palette than the default one where the colors are spread across the whole spectrum
+;; as much as possible, to avoid color colision as much as possible.
+;; (bright colors maybe too bright though)
+;; white palette because is build against a white background
+(define white-palette
+  #((0 0 0)
+    (0 139 255)
+    (139 255 0)
+    (255 0 139)
+    (115 115 115)
+    (255 115 0)
+    (0 255 115)
+    (115 0 255)
+    (115 231 208)
+    (208 115 231)
+    (231 208 115)
+    (162 0 23)
+    (23 162 0)
+    (0 23 162)
+    (115 0 139)
+    (231 0 255)
+    (0 139 115)
+    (0 255 231)
+    (139 115 0)
+    (255 231 0)
+    (255 115 139)
+    (115 139 255)
+    (139 255 115)
+    (0 0 255)
+    (0 255 0)
+    (255 0 0)
+    (69 69 208)
+    (69 208 69)
+    (208 69 69)
+    (46 69 69)
+    (185 185 185)
+    (185 23 185)
+    (162 185 46)
+    (23 185 185)
+    (92 46 0)
+    (92 185 139)
+    (139 92 185)
+    (23 0 92)
+    (185 231 255)
+    (231 255 185)
+    (255 185 231)
+    (185 139 115)
+    (0 115 185)
+    (185 0 115)
+    (0 92 0)
+    (139 69 255)
+    (69 255 139)
+    (255 139 69)
+    (92 0 69)
+    (69 231 0)
+    (208 255 46)
+    (92 139 46)
+    (46 208 255)
+    (255 46 208)
+    (139 46 92)
+    (46 92 139)
+    (255 0 69)
+    (0 69 255)
+    (208 162 0)
+    (208 46 0)
+    (0 208 46)
+    (46 0 208)
+    (69 139 208)
+    (208 69 139)
+    (115 185 0)
+    (255 185 162)
+    (255 255 92)
+    (185 162 255)
+    (255 92 255)
+    (92 255 255)
+    (162 255 185)
+    (0 69 115)
+    (69 115 0)
+    (46 255 46)
+    (162 115 69)
+    (185 231 139)
+    (231 139 185)
+    (139 185 231)
+    (231 185 46)
+    (69 23 115)
+    (115 162 185)
+    (115 69 46)
+    (139 162 92)
+    (139 208 162)
+    (162 139 208)
+    (0 46 46)
+    (23 115 46)
+    (115 231 46)
+    (185 92 23)
+    (23 185 92)
+    (92 23 185)
+    (46 23 23)
+    (185 115 162)
+    (139 0 208)
+    (0 208 139)
+    (208 46 231)
+    (46 231 208)
+    (255 69 46)
+    (69 46 255)
+    (92 255 92)
+    (92 115 162)
+    (69 115 255)
+    (255 69 115)
+    (69 139 115)
+    (115 69 139)
+    (185 231 0)
+    (162 92 115)
+    (162 46 139)
+    (46 139 162)
+    (208 139 46)
+    (255 162 115)
+    (23 255 162)
+    (115 255 162)
+    (162 23 255)
+    (162 115 255)
+    (46 69 0)
+    (115 0 0)
+    (0 185 231)
+    (162 208 92)
+    (231 0 185)
+    (92 69 92)
+    (185 23 69)
+    (69 185 23)
+    (23 46 208)
+    (162 255 69)
+    (92 185 255)
+    (255 92 185)
+    (46 46 162))
+  #;#((0 0 0)
+    (0 169 255)
+    (169 255 0)
+    (255 0 169)
+    (255 84 0)
+    (0 255 84)
+    (84 0 255)
+    (169 169 169)
+    (84 84 84)
+    (0 0 169)
+    (0 169 0)
+    (169 0 0)
+    (255 84 255)
+    (255 255 84)
+    (84 255 255)
+    (84 255 169)
+    (255 169 84)
+    (169 84 255)
+    (169 0 169)
+    (169 169 0)
+    (0 169 169)
+    (169 169 255)
+    (255 169 169)
+    (169 255 169)
+    (169 255 255)
+    (255 255 169)
+    (255 169 255)
+    (84 84 169)
+    (84 169 84)
+    (169 84 84)
+    (255 0 84)
+    (84 255 0)
+    (0 84 255)
+    (84 169 169)
+    (169 169 84)
+    (169 84 169)
+    (169 0 255)
+    (255 169 0)
+    (0 255 169)
+    (0 255 255)
+    (255 255 0)
+    (255 0 255)
+    (169 0 84)
+    (169 84 0)
+    (0 169 84)
+    (84 169 0)
+    (0 84 169)
+    (84 0 169)
+    (0 84 84)
+    (84 0 84)
+    (84 84 0)
+    (0 0 255)
+    (84 84 255)
+    (0 255 0)
+    (84 255 84)
+    (255 0 0)
+    (255 84 84)
+    (255 84 169)
+    (169 255 84)
+    (84 169 255)
+    (0 0 84)
+    (0 84 0)
+    (84 0 0)))
+
+(define (palette-ref idx #:palette [palette white-palette])
+  (vector-ref palette (modulo idx (vector-length palette))))
+
+(define (make-palette-auto-index #:palette [palette white-palette]
+                                 #:start [start 0])
+  (define idx start)
+  (λ()(begin0 (palette-ref idx #:palette palette)
+              (set! idx (add1 idx)))))
+
+(module+ drracket
+  (require slideshow racket/draw)
+
+  ;; Example of how to use a palette
+  (define get-new-color (make-palette-auto-index))
+  (apply
+   ht-append
+   (for/list ([color (+ 2 (vector-length white-palette))])
+     (colorize (filled-rectangle 20 20) (apply make-color (get-new-color)))))
+
+  ;; Helpers to create nice spread palettes
+
+  
+  (define (draw-palette palette
+                        #:width [width 20]
+                        #:border-width [border-width #f]
+                        #:background [background #f])
+    (apply
+     ht-append
+     (for/list ([color palette])
+       (colorize (filled-rectangle width width
+                                   #:border-width border-width
+                                   #:border-color (and background (apply make-color background)))
+                 (apply make-color color)))))
+
+  (require racket/math)
+  (define (color-dist c1 c2)
+    (sqrt (apply + (map (compose sqr -) c1 c2))))
+  ; use this to find most distant colors
+  (define (dist color palette)
+    (apply min
+           (for/list ([c palette] [i (in-naturals 1)])
+             (+ (color-dist c color) (/ (+ i 1.) i))))) ; small bonus for old colors
+
+  ;; build all color compositions of 0, 63, 127, 191, 255,
+  ;; start with white and recursively add the color that is the farther away for the current colors
+  ;; (i.e. the current best-response or greedy min-max optimal).
+  (define (best-color palette value-list)
+    (for*/fold ([best-color #f] [best-dist -1.])
+               ([r (in-list value-list)] [g (in-list value-list)] [b (in-list value-list)])
+      (define c (list r g b))
+      (define d (dist c palette))
+      (if (> d best-dist)
+          (values c d)
+          (values best-color best-dist))))
+
+  ;; Generates a high-contrast palette.
+  ;; n-values: number of different values to generat in the list.
+  ;; WARNING: Running time is cubic in the number of values! Choose a small number like 4 or maybe 10
+  (define (make-spread-palette n-values n-iter [pal '(#;(255 255 255))]
+                               #:background [background #f])
+    (define value-list
+      (build-list n-values (λ(i)(quotient (* 255 i) (- n-values 1)))))
+    (reverse
+     (for/fold ([pal pal])
+               ([iter n-iter])
+       (define-values (col dist)
+         (best-color (if background (cons background pal) pal) ; make background the most recent color
+                     value-list))
+       (if (> dist 1.) ; 1 because of bonus
+           (cons col pal)
+           pal))))
+
+  (for ([background '((255 255 255)
+                      (0 0 0)
+                      (0 127 127)
+                      (255 255 0)
+                      (255 0 0)
+                      (0 255 0)
+                      (0 0 255)
+                      )])
+    (println
+     (draw-palette (make-spread-palette
+                    10
+                    128
+                    #:background background)
+                   #:border-width 2
+                   #:background background)))
+
+  )
+
+
+
 
