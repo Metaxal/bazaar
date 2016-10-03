bc91ead187ca8758899e31995fc3183ddd61ed2f
Metaxal <laurent.orseau@gmail.com>
2016-10-03 09:50:53 +0100

    generalize var->name.var to multiple variables add in-range1, like in-range but offsetted by 1

--- base.rkt
+++ base.rkt
@@ -2,6 +2,10 @@
 
 (provide (all-defined-out))
 
+(module+ test
+  (require rackunit))
+
+
 (define (round-decimal x n)
   (define r (expt 10 n))
   (/ (round (* x r)) r))
@@ -14,6 +18,15 @@
   (values expr 'expr))
 
 ;; Returns the quoted variable name and its binding in an assoc
-(define-syntax-rule (var->name.var var)
-  (cons 'var var))
+(define-syntax-rule (vars->name.var var ...)
+  (list (cons 'var var) ...))
+
+(module+ test
+  (let ([x 3] [y 2])
+    (check-equal? (vars->name.var x y)
+                  '((x . 3) (y . 2)))))
+
+;; Like in-range, but offsetted by 1
+(define (in-range1 nmax)
+  (in-range 1 (+ nmax 1)))
 
