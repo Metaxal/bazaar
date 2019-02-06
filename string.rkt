d47e77203dabf18c2d2e18e3e5737b1e4b21ea8f
Metaxal <laurent.orseau@gmail.com>
2019-02-06 16:39:27 +0000

    add string->data

--- string.rkt
+++ string.rkt
@@ -2,6 +2,8 @@
 ;;; Copyright (C) Laurent Orseau, 2010-2013
 ;;; GNU Lesser General Public Licence (http://www.gnu.org/licenses/lgpl.html)
 
+(require racket/port)
+
 (provide (all-defined-out))
 
 (define (string?-ci=? a b)
@@ -17,3 +19,13 @@
 
 (define (string-or-false->string s [false-string ""])
   (or s false-string))
+
+;; Returns the lisp-like data contained in a string
+(define (string->data str)
+  (call-with-input-string
+   str
+   (λ(in) (let loop ([result '()])
+            (let ([v (read in)])
+              (if (eof-object? v)
+                  (reverse result)
+                  (loop (cons v result))))))))
