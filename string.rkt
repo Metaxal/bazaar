73577820d4500609620fbb95d2790caaebd853c5
Metaxal <laurent.orseau@gmail.com>
2020-01-31 09:37:29 +0000

    add on-error kw optional argument to string->data

--- string.rkt
+++ string.rkt
@@ -23,11 +23,21 @@
 (define (string-or-false->string s [false-string ""])
   (or s false-string))
 
-;; Returns the lisp-like data contained in a string
-(define (string->data str)
-  (call-with-input-string str (λ(in)(port->list read in))))
+;; Returns the lisp-like data contained in a string.
+;; When a read exception is caught, if on-error is a procedure of arity 1,
+;; it is called with the exception as argument, o.w. it is returned as a value.
+(define (string->data str #:on-error [on-error (λ (e) (raise e))])
+  (with-handlers ([exn:fail:read? (if (and (procedure? on-error)
+                                           (procedure-arity-includes? on-error 1))
+                                      on-error
+                                      (λ (e) on-error))])
+    (call-with-input-string str (λ(in)(port->list read in)))))
 
 (module+ test
   (check-equal? (string->data "((a)((\"bc\")(d . 5))) #(1 2 3)")
                 (list '((a) (("bc") (d . 5)))
-                      #(1 2 3))))
+                      #(1 2 3)))
+  (check-false (string->data " . " #:on-error #f))
+  (check-false (string->data " # " #:on-error #f))
+  (check-exn exn:fail:read? (λ () (string->data " # ")))
+  (check-exn exn:fail:read? (λ () (string->data " . "))))
