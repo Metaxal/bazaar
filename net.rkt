23598932ea90276dbf4d0fde2d56ae7f770acc82
Metaxal <laurent.orseau@gmail.com>
2021-03-22 14:57:16 +0000

    add url-bust-cache

--- net.rkt
+++ net.rkt
@@ -1,14 +1,26 @@
 #lang racket/base
 
 (require net/url
-         racket/port
-         racket/contract)
+         racket/contract
+         racket/string
+         racket/port)
 
-(provide (contract-out [url-contents ((or/c url? string?) . -> . string?)]))
+(provide (contract-out [url-contents ((or/c url? string?) . -> . string?)]
+                       [url-bust-cache (url? . -> . url?)]))
+
+;; Sometimes an intermediate node may still serve a cached version despite
+;; headers. To prevent this, we simply add some noise in the url.
+(define (url-bust-cache aurl)
+  (struct-copy url aurl
+               [query (cons (cons 'cachebust (number->string (random 4294967087)))
+                            (url-query aurl))]))
 
 ;; Accepts a string representing a URL and returns the corresponding raw webpage.
-(define (url-contents url/str)
-  (port->string (get-pure-port (if (url? url/str)
-                                   url/str
-                                   (string->url url/str)))))
+(define (url-contents url/str #:bust-cache? [bust-cache? #false])
+  (let ([aurl (if (url? url/str)
+                url/str
+                (string->url url/str))])
+    (port->string (get-pure-port (if bust-cache? (url-bust-cache aurl) aurl)
+                                 #:redirections 10)
+                  #:close? #true)))
 
