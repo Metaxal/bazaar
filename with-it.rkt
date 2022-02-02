fb31260929211efa2f8997c21d421bf6dc0a80dd
Metaxal <laurent.orseau@gmail.com>
2022-02-02 09:15:35 +0000

    add λ/with

--- with-it.rkt
+++ with-it.rkt
@@ -2,7 +2,7 @@
 (require (for-syntax racket/base
                      syntax/parse))
 
-(provide with)
+(provide with λ/with (rename-out [λ/with lambda/with]))
 
 (define-syntax (with stx)
   (syntax-parse stx
@@ -12,6 +12,9 @@
      #'(let ([it body1])
          (with it body2 ...))]))
 
+(define-syntax-rule (λ/with it body ...)
+  (λ (x) (with it x body ...)))
+
 ;;; Example
 (module+ drracket
   (require racket)
@@ -20,4 +23,16 @@
         (string-split it "\n")
         (filter (λ (line) (string-contains? line "doggy")) it)
         (sort it string<?)
-        (remove-duplicates it)))
+        (remove-duplicates it))
+
+  (define s
+    "\
+:black_large_square::black_large_square::large_yellow_square::large_yellow_square::black_large_square:
+:large_yellow_square::black_large_square::black_large_square::large_yellow_square::black_large_square:
+:black_large_square::black_large_square::large_yellow_square::black_large_square::black_large_square:
+:large_green_square::large_green_square::large_green_square::large_green_square::large_green_square:")
+  (map (λ/with it
+               (string-replace it ":large_green_square:" "G" #:all? #t)
+               (string-replace it ":black_large_square:" "B" #:all? #t)
+               (string-replace it ":large_yellow_square:" "O" #:all? #t))
+       (string-split s "\n")))
